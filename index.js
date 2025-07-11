60f924733580dfc9f5c08349c9d01be1ff684c18
Daniel Velnic <velnicd@yahoo.com>
2025-07-11 18:57:17 +0300

    57 - JSON response

--- index.js
+++ index.js
@@ -13,9 +13,29 @@ app.post("/two", function(req, res) {
     res.send("This is post simple response");
 });
 
+// Response status code
 app.get("/three", function(req, res) {
     // 401 - unathorized 
     res.status(401).end("You don't have any permission");
+});
+
+// JSON response 
+app.get("/four", function(req, res) {
+    let myJSONArray = [
+        {
+            name: "Ariyan",
+            city: "Dhaka"
+        },
+        {
+            name: "Raju",
+            city: "USA"
+        },
+        {
+            name: "Johny",
+            city: "India"
+        },
+    ]
+    res.json(myJSONArray);
 });
 
 app.listen(8000, function() {
