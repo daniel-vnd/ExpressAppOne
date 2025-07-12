f024c83ace0697061dd1e258c99cb5a48058dec6
Daniel Velnic <velnicd@yahoo.com>
2025-07-12 12:04:18 +0300

    69 - Post Application JSON

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 
 // Create the app
 app = express();
 
+// Use Body Parser and call it as JSON method
+app.use(bodyParser.json());
 
+// https://www.npmjs.com/package/body-parser
 app.post("/", function(req, res) {
-    let userName = req.header("userName");
-    let password = req.header("password");
-    
-    res.send("User Name: " + userName + " Password: " + password);
+    let JSONData = req.body;
+    let JSONString = JSON.stringify(JSONData);
+
+    res.send(JSONString);
 });
 
 app.listen(8000, function() {
