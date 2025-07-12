62ec488d160b6fe7d5b43bafdb9ad2368861f335
Daniel Velnic <velnicd@yahoo.com>
2025-07-12 12:29:15 +0300

    71 - File Upload Success

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 var multer = require('multer');
-var multer  = require('multer');
-
-var multer = multer();
 
 // Create the app
 app = express();
-app.use(multer.array());
-app.use(express.static('public'));
+
+var storage = multer.diskStorage({
+    // Where the destination will be uploaded
+    destination: function(req, file, callback){
+        callback(null, './uploads');
+    },
+    // In particular with file original name
+    filename: function(req, file, callback) {
+        callback(null, file.originalname)
+    }
+});
+
+var upload = multer({storage: storage}).single('myfile');
 
 // https://www.npmjs.com/package/body-parser
 app.post("/", function(req, res) {
-    let JSONData = req.body;
 
-    res.send(JSON.stringify(JSONData));
+    upload(req, res, function(error) {
+        if (error) {
+            res.send("File Upload Failed");
+        } else {
+            res.send("File Upload Success");
+        }
+    })
 });
 
 app.listen(8000, function() {
