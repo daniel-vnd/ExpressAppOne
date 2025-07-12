197cbdeee42b9aaeac27fd9ad5e05520dcd6b20d
Daniel Velnic <velnicd@yahoo.com>
2025-07-12 11:07:06 +0300

    64 - Get Request With URL Query

--- index.js
+++ index.js
@@ -2,89 +2,14 @@ var express = require('express');
 
 // Create the app
 app = express();
-/*
-app.get("/one", function(req, res) {
-    // send - method work for the response of your body 
-    // end - response your ending point 
-    res.end("This is simple string response"); 
+
+// In browser/postman write: http://localhost:8000?firstName=Daniel&lastName=Nicolae
+app.get("/", function(req, res) {
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+
+    res.end(firstName + " " + lastName); 
 });
-*/
-
-app.post("/two", function(req, res) {
-    res.send("This is post simple response");
-});
-
-// Response status code
-app.get("/three", function(req, res) {
-    // 401 - unathorized 
-    res.status(401).end("You don't have any permission");
-});
-
-// JSON response 
-app.get("/four", function(req, res) {
-    let myJSONArray = [
-        {
-            name: "Ariyan",
-            city: "Dhaka"
-        },
-        {
-            name: "Raju",
-            city: "USA"
-        },
-        {
-            name: "Johny",
-            city: "India"
-        },
-    ]
-    res.json(myJSONArray);
-});
-
-// 58 - Response Download
-app.get("/five", function(req, res) {
-    res.download("./uploads/about.jpg"); 
-});
-
-// 59 - Response Redirect
-app.get("/usa", function(req, res) {
-    res.redirect("http://localhost:8000/india"); 
-});
-
-
-app.get("/india", function(req, res) {
-    res.send("This is india page"); 
-});
-
-// 60 - Response Header
-app.get("/six", function(req, res) {
-    res.append("name", "Kazy Ariyan"); 
-    res.append("city", "Dhaka"); 
-
-    res.status(201).end("This is Response Header");
-});
-
-// 61 - Response Set Cookies Data
-// In browser go in debug(F12)->Application->Cookies
-app.get("/seven", function(req, res) {
-    res.cookie("name", "Kazy Ariyan"); 
-    res.cookie("city", "Dhaka"); 
-    res.cookie("age", "35 Years Old"); 
-
-    res.end("Cookie Set Successfully");
-});
-
-// 62 - Response Delete Cookies Data
-app.get("/eight", function(req, res) {
-    res.clearCookie("name"); 
-    res.clearCookie("city"); 
-
-    res.end("Cookie Deleted Successfully");
-});
-
-// 63 - Working with Request
-// Post() : work with post data, with url and parameter, request header, body, multipart/form-data
-// Get() : create simple get request(simple or with url or parameter), catch 
-// Put()
-// Delete()
 
 app.listen(8000, function() {
     console.log("Server Run Success");
