c4d663b40fe42861a7108813b3551f351ae12f3d
Daniel Velnic <velnicd@yahoo.com>
2025-07-12 11:25:48 +0300

    66 - Simple Post Request - deleted wrong comment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 // Create the app
 app = express();
 
-// In browser/postman write: http://localhost:8000?firstName=Daniel&lastName=Nicolae
+
 app.post("/", function(req, res) {
     res.send('This is simple Post Request')
 });
